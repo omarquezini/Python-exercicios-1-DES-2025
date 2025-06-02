994035692bc387bf5e492978b284db2fd4c04419
omarquezini <rafael.marquezini.silva@escola.pr.gov.br>
2025-06-02 14:07:33 +0000

    organizando ascoisas.

--- exercicio02.py
+++ exercicio02.py
@@ -7,9 +7,9 @@ intregaX = int(input("digite o tempo restante da intragaX. "))
 intregaY = int(input("digite o tempo restante da intragaY. "))
 intregaZ = int(input("digite o tempo restante da entregaZ. "))
 
-soma = intregaX + intregaY + intregaZ
 
 if intregaX < 0 or intregaY < 0 or intregaZ < 0:
     print("ERRO.")
-elif soma >= 0:
+else:
+    soma = intregaX + intregaY + intregaZ
     print(f"{soma}")
